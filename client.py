1b72d84ba13161c18815eeac68086c4f61db26ab
orangefalcon3 <akhilan@gurumoorthy.com>
2025-01-09 09:31:26 -0800

    Functionality for Multiple Jobs

--- client.py
+++ client.py
@@ -9,12 +9,36 @@ class Client:
     def request_status(self):
         response = self.http.request("GET", self.server_url + "/status")
         return response
+    
+    def create_job(self):
+        response = self.http.request("GET", self.server_url + "/create")
+        return response
+    
+    def start_job(self):
+        response = self.http.request("GET", self.server_url + "/start")
+        return response
         
 if __name__ == "__main__":
     client = Client("http://localhost:8000")
+    curr_job_status = "N/A"
     while True:
         response = client.request_status().json()
-        print(response["result"])
+        if response["result"] != curr_job_status:
+            print("Job Status has been changed to " + str(response["result"]))
+            curr_job_status = response["result"]
+        if response["result"] == "completed":
+            break
+        time.sleep(1)
+    curr_job_status = "N/A"
+    response = client.create_job().json()
+    print(response["result"])
+    response = client.start_job().json()
+    print(response["result"])
+    while True:
+        response = client.request_status().json()
+        if response["result"] != curr_job_status:
+            print("Job Status has been changed to " + str(response["result"]))
+            curr_job_status = response["result"]
         if response["result"] == "completed":
             break
         time.sleep(1)
--- server.py
+++ server.py
@@ -1,20 +1,61 @@
 import time
 from fastapi import FastAPI
 
-class Job:
+class JobHandler:
     def __init__(self):
-        self.start = time.time()
-        self.job_length = 50 #Temporary Placeholder, should be configurable?
+        self.jobs = []
+        self.current_job = Job(10)
+        self.current_job.start_job()
     
+    def create_job(self, length=10):
+        newJob = Job(length)
+        self.jobs.append(newJob)
+    
+    def start_job(self):
+        if (not self.current_job or self.current_job.status() == "completed"):
+            if (len(self.jobs) > 0):
+               self.current_job = self.jobs.pop(0)
+               self.current_job.start_job()
+               return "New Job has started"
+            else:
+                return "Error: no jobs in queue"
+        else:
+            return "Error: another job is currently running"
+    
+    def get_current_status(self):
+        if (not self.current_job):
+            return "error"
+        return self.current_job.status()
+        
+class Job:
+    def __init__(self, length=10):
+        self.start = 0
+        self.job_length = length 
+    
+    def start_job(self):
+        self.start = time.time()
+
     def status(self):
+        if self.start == 0:
+            return "pending"
         elapsed_time = time.time() - self.start
         if elapsed_time >= self.job_length:
             return "completed"
         return "pending"
 
 server = FastAPI()
-job = Job()
+handler = JobHandler()
+
 
 @server.get("/status")
-async def get_status():
-    return {"result": job.status()}
+async def get_current_status():
+    return {"result": handler.get_current_status()}
+
+@server.get("/start")
+async def start_job():
+    return {"result": handler.start_job()}
+
+@server.get("/create")
+async def create_job():
+    return {"result": handler.create_job()}
+
