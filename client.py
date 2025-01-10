7d6b78a9fafdba1da7f0a0a0f0026b6e5f254dcb
orangefalcon3 <akhilan@gurumoorthy.com>
2025-01-10 12:25:25 -0800

    Initial Setup for Rate Limiting

--- client.py
+++ client.py
@@ -6,39 +6,40 @@ class Client:
         self.server_url = server_url
         self.http = urllib3.PoolManager()
 
+    # Request the status of the current job
     def request_status(self):
         response = self.http.request("GET", self.server_url + "/status")
         return response
     
+    # Create a new job and add it to the job queue
     def create_job(self):
         response = self.http.request("GET", self.server_url + "/create")
         return response
     
+    # Start the next job in the queue
     def start_job(self):
         response = self.http.request("GET", self.server_url + "/start")
         return response
+    
+    # Continuously get the status of the job until it is completed
+    def monitor_job(self): 
+        curr_job_status = "N/A"
+        while curr_job_status != "completed" and curr_job_status != "error":
+            response = self.request_status().json()
+            if response["result"] != curr_job_status:
+                print("Job Status is now \"" + str(response["result"]) + "\"")
+                curr_job_status = response["result"]
+
         
 if __name__ == "__main__":
     client = Client("http://localhost:8000")
-    curr_job_status = "N/A"
-    while True:
-        response = client.request_status().json()
-        if response["result"] != curr_job_status:
-            print("Job Status has been changed to " + str(response["result"]))
-            curr_job_status = response["result"]
-        if response["result"] == "completed":
-            break
-        time.sleep(1)
-    curr_job_status = "N/A"
+    
+    client.monitor_job()
+    
     response = client.create_job().json()
     print(response["result"])
+    
     response = client.start_job().json()
     print(response["result"])
-    while True:
-        response = client.request_status().json()
-        if response["result"] != curr_job_status:
-            print("Job Status has been changed to " + str(response["result"]))
-            curr_job_status = response["result"]
-        if response["result"] == "completed":
-            break
-        time.sleep(1)
+    
+    client.monitor_job()
